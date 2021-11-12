ef5832e8bc437f894fea8b0cdde69ae8ce7665c9
Unix13 <vivasjesus80@gmail.com>
2021-11-12 18:37:03 +0100

    Creo getters y setters

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -1,9 +1,37 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class Torre {
+	
+	
 	// Atributos
-		private Color color;
-		private Posicion posicion;
+	private Color color;
+	private Posicion posicion;
 
+	// getters y setters
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		}
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Posicion posicion) {
+
+		if (posicion == null) {
+			throw new IllegalArgumentException("La posición no es la esperada.");
+		} else {
+
+			this.posicion = posicion;
+		}
+	}
 
 }
