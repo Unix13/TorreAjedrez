7ac2d6a664acf26b7b1885cd27407ff39ab7b622
Unix13 <vivasjesus80@gmail.com>
2021-11-12 18:41:25 +0100

    Creo métodos hashCode y equals

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import java.util.Objects;
+
 import javax.naming.OperationNotSupportedException;
 
 public class Torre {
@@ -196,6 +198,25 @@ public class Torre {
 		default:
 			break;
 		}
+	}
+
+	// Metodos hashCade y equals
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(color, posicion);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Torre other = (Torre) obj;
+		return color == other.color && Objects.equals(posicion, other.posicion);
 	}
 
 }
