984404822ee516168f9f4718fed83192b661d8ed
Unix13 <vivasjesus80@gmail.com>
2021-11-12 18:28:53 +0100

    Creo getters y setters

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -1,9 +1,40 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class Posicion {
-	
+
 	private int fila;
 	private char columna;
-	
+
+	public int getFila() {
+		return fila;
+	}
+
+	public void setFila(int fila) {
+
+		if (fila >= 1 && fila <= 8) {
+
+			this.fila = fila;
+
+		} else {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	public void setColumna(char columna) {
+
+		if (Character.toLowerCase(columna) >= 'a' && Character.toLowerCase(columna) <= 'h') {
+
+			this.columna = columna;
+
+		} else {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
+
+	}
 
 }
