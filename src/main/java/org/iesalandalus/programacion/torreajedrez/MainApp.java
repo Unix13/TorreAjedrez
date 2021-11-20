d7858ae1ed7eca17811a765f9f13d7335889ca23
Unix13 <vivasjesus80@gmail.com>
2021-11-20 11:13:06 +0100

    Método mostrarMenu

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -1,13 +1,16 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
 
 	private static Torre torre;
 
 	public static void main(String[] args) {
 
-		
-	// Metodo mostrarTorre
+	// Método mostrarTorre
 	private static void mostrarTorre() {
 		try {
 			System.out.println(torre.toString());
@@ -15,4 +18,16 @@ public class MainApp {
 			System.out.println("ERROR: La torre no está creada.");
 		}
 	}
+
+	// Método mostrarMenu
+	private static void mostrarMenu() {
+		System.out.println(" ");
+		System.out.println("1. Crear una torre por defecto.");
+		System.out.println("2. Crear una torre eligiendo su color.");
+		System.out.println("3. Crear una torre eligiendo su color y posición inicial.");
+		System.out.println("4. Mover la torre.");
+		System.out.println("5. Salir.");
+	}
+
+	
 }
